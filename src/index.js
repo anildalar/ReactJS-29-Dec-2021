5b4eacceac6a7dc06508afd485f93dd7c98fdb37
“anildalar” <dollorinfotech@gmail.com>
2021-12-29 09:37:49 +0530

    v1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
+// Import Area
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//Let Create another RFC
+function Contact(){
+  return <span>+91-7999452711</span>
+}
+
+//Let create our first RFC = F = Functio
+function Course({mentor}){
+  return (
+    <span className="a">Join OKLABS Training For ReactJS { mentor } <Contact /> </span>
+  );
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Welcome To ReactJS <Course mentor={'ANIL DOLLOR'}  /> </h1>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('anil')
 );
 
 // If you want to start measuring performance in your app, pass a function
